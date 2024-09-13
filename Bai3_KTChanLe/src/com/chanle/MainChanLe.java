30fe12a43fac5cdefe4345ffdf9f1316c3411a16
ptai82067 <ptai82067@gmail.com>
2024-09-13 14:51:40 +0700

    Kiểm tra chẵn lẻ

---
+++ Bai3_KTChanLe/src/com/chanle/MainChanLe.java
@@ -0,0 +1,31 @@
+package com.chanle;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class MainChanLe {
+    public static void main(String[] args) {
+        int a;
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            try {
+                System.out.println("Nhập một số nguyên: ");
+                a = scanner.nextInt();
+                break; // Thoát khỏi vòng lặp nếu nhập đúng số nguyên
+            } catch (InputMismatchException e) {
+                System.out.println("Vui lòng nhập một số nguyên hợp lệ.");
+                scanner.next(); // Xóa đầu vào không hợp lệ
+            }
+        }
+        
+        String border = "====================";
+        System.out.println(border);
+        if (a % 2 == 0) {
+            System.out.println("Đây là số chẵn.");
+        } else {
+            System.out.println("Đây là số lẻ.");
+        }
+        System.out.println(border);
+    }
+}
---
+++ Bai3_KTChanLe/src/module-info.java
@@ -0,0 +1,2 @@
+module Bai3_KTChanLe {
+}
