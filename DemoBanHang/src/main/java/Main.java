0bb0f25bad61b95bf8347e95722f9830a47aef80
ptai82067 <ptai82067@gmail.com>
2024-10-22 16:33:23 +0700

    MySQL-DemoBanHang

---
+++ DemoBanHang/src/main/java/Main.java
@@ -0,0 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class Main {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//đăng kí driver
+		Class.forName("com.mysql.cj.jdbc.Driver");//com.mysql.jdbc.Driver
+		//mở kết nối
+		//chuẩn bị chuỗi kết nối
+		String connectionURL = "jdbc:mysql://localhost:3306/demobanhang";
+		// mở kế nối
+		Connection con = DriverManager.getConnection(connectionURL,"root","");
+		System.out.println("haha");
+		// select
+		String sqlSelect = "SELECT * FROM SANPHAM";
+		Statement lenh = con.createStatement();
+		ResultSet ketQua = lenh.executeQuery(sqlSelect);
+		// hiện kết quả
+		while(ketQua.next()) {
+			System.out.println("Id: "+ketQua.getInt("id") + " Tên sản phẩm: "+ketQua.getString("TenSP")
+			+ " Giá SP: "+ketQua.getFloat("GiaSP")
+			+ " Mô tả: "+ketQua.getString("MoTa"));
+			
+		}
+	}
+}
