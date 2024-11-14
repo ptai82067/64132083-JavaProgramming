27cfc3d7a14a78d1dfb3fbe9de6c7fe66951348a
ptai82067 <ptai82067@gmail.com>
2024-11-14 09:15:13 +0700

    Dự án thi cuối kì

---
+++ ProjectCuoiKi/ProjectFinalExam/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module vn.edu.taipp.projectfinalexam {
+  requires javafx.controls;
+  requires javafx.fxml;
+
+
+  opens vn.edu.taipp.projectfinalexam to javafx.fxml;
+  exports vn.edu.taipp.projectfinalexam;
+}
