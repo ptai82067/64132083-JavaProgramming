f37c183092d4264a354f5ea79ea6da05163750e5
ptai82067 <ptai82067@gmail.com>
2024-11-15 14:21:38 +0700

    Push2h20

--- OutCome5_11/Shape/src/main/java/vn/edu/taipp/shape/HelloApplication.java
+++ OutCome5_11/Shape/src/main/java/vn/edu/taipp/shape/HelloApplication.java
@@ -35,7 +35,7 @@ public class HelloApplication extends Application {
         leftEar.setStroke(Color.BLACK);
         leftEar.setFill(Color.WHITE);
 
-        // Vẽ tai phải
+        // Vẽ tai phảin 
         Polygon rightEar = new Polygon();
         rightEar.getPoints().addAll(
                 200.0, 100.0,
@@ -45,7 +45,44 @@ public class HelloApplication extends Application {
         rightEar.setStroke(Color.BLACK);
         rightEar.setFill(Color.WHITE);
 
+        // Vẽ tam giác nhỏ bên trong
+        Polygon innerTriangle = new Polygon();
+        innerTriangle.getPoints().addAll(
+                150.0, 120.0,
+                130.0, 150.0,
+                170.0, 150.0
+        );
+        innerTriangle.setStroke(Color.RED);
+        innerTriangle.setFill(Color.TRANSPARENT);
 
+        // Vẽ thân hình (hình cung)
+        Arc bodyArc = new Arc(150, 230, 100, 50, 0, 180);
+        bodyArc.setStroke(Color.BLACK);
+        bodyArc.setFill(Color.TRANSPARENT);
+
+        // Vẽ chân trái
+        Circle leftFoot = new Circle(110, 250, 20);
+        leftFoot.setStroke(Color.BLACK);
+        leftFoot.setFill(Color.TRANSPARENT);
+
+        // Vẽ chân phải
+        Circle rightFoot = new Circle(190, 250, 20);
+        rightFoot.setStroke(Color.BLACK);
+        rightFoot.setFill(Color.TRANSPARENT);
+
+        // Vẽ trang trí trên chân trái (hình cung)
+        Arc leftFootDetail = new Arc(110, 250, 10, 10, 0, 180);
+        leftFootDetail.setStroke(Color.RED);
+        leftFootDetail.setFill(Color.TRANSPARENT);
+
+        // Vẽ trang trí trên chân phải (hình cung)
+        Arc rightFootDetail = new Arc(190, 250, 10, 10, 0, 180);
+        rightFootDetail.setStroke(Color.RED);
+        rightFootDetail.setFill(Color.TRANSPARENT);
+
+        // Thêm tất cả các phần tử vào pane
+        pane.getChildren().addAll(mainTriangle, leftEar, rightEar, innerTriangle,
+                bodyArc, leftFoot, rightFoot, leftFootDetail, rightFootDetail);
         Scene scene = new Scene(pane, 300, 300);
         stage.setTitle("Vẽ Hình");
         stage.setScene(scene);
